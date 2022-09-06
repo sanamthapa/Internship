557e0339d309077be2d4bc5ab30f5e438201df34
Sanam Thapa <thapasanam82@gmail.com>
2022-09-06 17:49:03 +0545

    Add project files.

---
+++ Day1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship
+{
+    internal class Day1
+    {
+        static void Main(string[] args)
+        {
+            AmnilInterns name = new AmnilInterns();
+            name.Set("Rabin Shrestha", "Ktm", "BCA\n");
+            AmnilInterns name1 = new AmnilInterns();
+            name1.Set("Sanam Thapa", "Bhaktapur", "CSIT\n");
+            AmnilInterns name2 = new AmnilInterns();
+            name2.Set("Rajendra Lungeli", "Bhaktapur", "BIT\n");
+            AmnilInterns name3 = new AmnilInterns();
+            name3.Set("Sandip Sharma", "Sanepa", "Computer Engineering\n");
+            AmnilInterns name4 = new AmnilInterns();
+            name4.Set("Bibek Neupane", "Duwakot", "CSIT\n");
+            name.get();
+            name1.get();
+            name2.get();
+            name3.get();
+            name4.get();
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship
+{
+
+    public class AmnilInterns
+    {
+
+        public string Name { get; set; } = default!;
+        public string Address { get; set; } = default!;
+
+        public string Faculty { get; set; } = default!;
+
+        public void Set(string name, string address, string faculty)
+        {
+
+            this.Name = name;
+            this.Address = address;
+            this.Faculty = faculty;
+
+        }
+        public void get()
+        {
+            Console.WriteLine("Name =" + " " + Name);
+            Console.WriteLine("Address =" + " " + Address);
+            Console.WriteLine("Faculty =" + " " + Faculty);
+        }
+    }
+}
+        
+
+
+
+    
